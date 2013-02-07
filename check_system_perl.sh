9e2e5d7ba2cf0997ef0de097ebd7f6f0421f5ebb
Marco Ferrufino <cpanelmarco@yahoo.com>
2013-02-07 08:35:56 -0600

    changing echos to printf

--- check_system_perl.sh
+++ check_system_perl.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 # bash script to verify system perl location is correct
 # does not work for cpanel perl rpm yet
+# only designed for supported server OS's
 # http://staffwiki.cpanel.net/LinuxSupport/PerlIssues
 
 # Colors and formatting
@@ -11,7 +12,7 @@ redbold='\033[1;31m'
 
 serverenv=$(strings -1 /var/cpanel/envtype)
 
-printf "cPanel claims this server is a $serverenv server, so we'll test it as such.\n"
+printf "cPanel claims this server is a $serverenv server, so we'll test it as such.\n\n"
 
 
 # set the two link vars.  if they are links, these vars will be full
@@ -23,44 +24,44 @@ verdict=1
 if [ $serverenv = "standard" ]; then
 
  if [ $ubp ]; then
-  echo "/usr/bin/perl is a link to $ubp. This is not correct. Just to check, here it is:";
+  printf "/usr/bin/perl is a link to $ubp. This is not correct. Just to check, here it is:\n";
   ls -la /usr/bin/perl;
   verdict=2;
  fi
  
  if [ ! $ulbp ]; then
-  echo "/usr/local/bin/perl is not a link. This is not correct. Just to check, here it is:";
+  printf "/usr/local/bin/perl is not a link. This is not correct. Just to check, here it is:\n";
   ls -la /usr/local/bin/perl;
   verdict=2;
  fi
  
  if [[ ! $(perl -v | grep thread) ]];
-  then echo "System perl is not threaded.  This is not correct";
+  then printf "System perl is not threaded.  This is not correct";
   verdict=2;
  fi
 
 elif [ $serverenv ]; then
 
  if [ ! $ubp ]; then
-  echo "/usr/bin/perl is not a link. This is not correct.  Just to check, here it is:";
+  printf "/usr/bin/perl is not a link. This is not correct.  Just to check, here it is:\n";
   ls -la /usr/bin/perl;
   verdict=2;
  fi
  
  if [ $ulbp ]; then
-  echo "/usr/local/bin/perl is a link. This is not correct. Just to check, here it is:";
+  printf "/usr/local/bin/perl is a link. This is not correct. Just to check, here it is:\n";
   ls -la /usr/local/bin/perl;
   verdict=2;
  fi
  
  if [[ $(perl -v | grep thread) ]]; then
-  echo "System perl is threaded.  This is not correct.";
+  printf "System perl is threaded.  This is not correct.\n";
   verdict=2;
  fi
 
 else
 
- echo "There was a problem determining server environment.  You can try installing & running 'virt-what'.  Also, please let Marco know."
+ printf "There was a problem determining server environment.  You can try installing & running 'virt-what'.  Also, please let Marco know.\n"
  verdict=2;
 
 fi
