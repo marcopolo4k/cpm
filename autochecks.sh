acadb223ac98f1b5726c8779575bcf67c578ff86
Marco Ferrufino <cpanelmarco@yahoo.com>
2013-02-18 12:28:09 -0600

    added one line to libkey

--- autochecks.sh
+++ autochecks.sh
@@ -195,5 +195,5 @@ assiciated_rpm=$(rpm -qf "/"$libkey_dir"/"$libkey_ver)
 assiciated_rpm_check=$(echo $assiciated_rpm | grep "is not owned by any package")
 if [ "$assiciated_rpm_check" ]; then
     echo -e $red"libkey check failed. rpm associated with libkey file: "$clroff
-    echo $assiciated_rpm
 fi
+echo $assiciated_rpm
