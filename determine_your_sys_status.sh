395604a0a48d68bda8c796148895f5f94fa88313
Marco Ferrufino <cpanelmarco@yahoo.com>
2013-03-01 07:28:14 -0600

    changed wording & spacing

--- determine_your_sys_status.sh
+++ determine_your_sys_status.sh
@@ -25,41 +25,49 @@ if [ "$assiciated_rpm_check" ]; then
 fi
 
 # Command 1
+echo "Command 1 Test:"
 keyu_pckg_chg_test=$(rpm -V keyutils-libs)
 if [ "$keyu_pckg_chg_test" ]; then
-    echo -e "\n"$red"Cmd1: keyutils-libs check failed. The rpm shows the following file changes: "$clroff
+    echo -e "\n"$red"keyutils-libs check failed. The rpm shows the following file changes: "$clroff
     echo $keyu_pckg_chg_test
     num_fails=$((num_fails+1))
 fi
 
 # Command 2
+echo "Command 2 Test:"
 cmd_2_chk=$(\ls -la $thelibkey | egrep "so.1.9|so.1.3.2|1.2.so.2");
 if [ "$cmd_2_chk" ]; then
-    echo -e "\n"$red"Cmd2: Known bad package check failed. The following file is linked to libkeyutils.so.1: "$clroff
+    echo -e "\n"$red"Known bad package check failed. The following file is linked to libkeyutils.so.1: "$clroff
     echo $cmd_2_chk
     num_fails=$((num_fails+1))
 fi
 
 # Command 3
+echo "Command 3 Test:"
 cmd_3_chk=$(strings $thelibkey | egrep 'connect|socket|inet_ntoa|gethostbyname')
 if [ "$cmd_3_chk" ]; then
-    echo -e "\n"$red"Cmd3: libkeyutils libraries contain networking tools: "$clroff
+    echo -e "\n"$red"libkeyutils libraries contain networking tools: "$clroff
     echo $cmd_3_chk
     num_fails=$((num_fails+1))
 fi
 
 # Command 4
+echo "Command 4 Test:"
 check_ipcs_lk=$(for i in `ipcs -mp | grep -v cpid | awk {'print $3'} | uniq`; do ps aux | grep $i | grep -v grep;done | grep -i ssh)
 if [ "$check_ipcs_lk" ];
- then echo -e "\n"$red"Cmd4: IPCS Check failed.  Doesn't necessarily mean anything:\n"$clroff$check_ipcs_lk;
+ then echo -e "\n"$red"IPCS Check failed.  Doesn't necessarily mean anything:\n"$clroff$check_ipcs_lk;
  num_fails=$((num_fails+1))
 fi
 
+# Command 5
+echo "Command 5 Test is not designed to run by this automated script"
+
 # Command 6
+echo "Command 6 Test:"
 for i in $(ldd /usr/sbin/sshd | cut -d" " -f3); do
  sshd_library=$(rpm -qf $i);
  if [ ! "sshd_library" ]; then
-  echo -e "\nCmd 6: "$i" has no associated library."; echo $sshd_library;
+  echo -e "\n"$i" has no associated library."; echo $sshd_library;
   num_fails=$((num_fails+1))
  fi;
 done
@@ -72,7 +80,7 @@ if [ "$num_fails" -gt 0 ]; then
    stat $i | grep -i change;
   fi;
  done
- echo -e "\nTotal Number of checks failed: "$num_fails
+ echo -e "\nTotal Number of checks failed: "$num_fails" (out of 7 checks currently)"
 fi
 
 # Just for info:
