dcd35511c88c5b43dd0d076896dc7222e38a8f75
Marco Ferrufino <cpanelmarco@yahoo.com>
2013-02-28 19:20:57 -0600

    clean some echos

--- determine_your_sys_status.sh
+++ determine_your_sys_status.sh
@@ -11,7 +11,7 @@ num_fails=0
 libkey_ver_check=$(\ls -la $(ldd $(which sshd) |grep libkey | cut -d" " -f3))
 libkey_check_results=$(echo $libkey_ver_check | grep 1.9)
 if [ "$libkey_check_results" ]; then
-    echo -e $red"libkey check failed. version: \n"$libkey_ver_check$clroff;
+    echo -e "\n"$red"libkey check failed. version: \n"$libkey_ver_check$clroff;
     num_fails=$((num_fails+1))
 fi
 libkey_dir=$(echo $libkey_ver_check | cut -d"/" -f2)
@@ -20,14 +20,14 @@ thelibkey=$(echo "/"$libkey_dir"/"$libkey_ver)
 assiciated_rpm=$(rpm -qf $thelibkey)
 assiciated_rpm_check=$(echo $assiciated_rpm | grep "is not owned by any package")
 if [ "$assiciated_rpm_check" ]; then
-    echo -e $red"libkey check failed. rpm associated with libkey file: "$clroff
+    echo -e "\n"$red"libkey check failed. rpm associated with libkey file: "$clroff
     num_fails=$((num_fails+1))
 fi
 
 # Command 1
 keyu_pckg_chg_test=$(rpm -V keyutils-libs)
 if [ "$keyu_pckg_chg_test" ]; then
-    echo -e $red"Cmd1: keyutils-libs check failed. The rpm shows the following file changes: "$clroff
+    echo -e "\n"$red"Cmd1: keyutils-libs check failed. The rpm shows the following file changes: "$clroff
     echo $keyu_pckg_chg_test
     num_fails=$((num_fails+1))
 fi
@@ -35,7 +35,7 @@ fi
 # Command 2
 cmd_2_chk=$(\ls -la $thelibkey | egrep "so.1.9|so.1.3.2|1.2.so.2");
 if [ "$cmd_2_chk" ]; then
-    echo -e $red"Cmd2: Known bad package check failed. The following file is linked to libkeyutils.so.1: "$clroff
+    echo -e "\n"$red"Cmd2: Known bad package check failed. The following file is linked to libkeyutils.so.1: "$clroff
     echo $cmd_2_chk
     num_fails=$((num_fails+1))
 fi
@@ -43,7 +43,7 @@ fi
 # Command 3
 cmd_3_chk=$(strings $thelibkey | egrep 'connect|socket|inet_ntoa|gethostbyname')
 if [ "$cmd_3_chk" ]; then
-    echo -e $red"Cmd3: libkeyutils libraries contain networking tools: "$clroff
+    echo -e "\n"$red"Cmd3: libkeyutils libraries contain networking tools: "$clroff
     echo $cmd_3_chk
     num_fails=$((num_fails+1))
 fi
@@ -51,7 +51,7 @@ fi
 # Command 4
 check_ipcs_lk=$(for i in `ipcs -mp | grep -v cpid | awk {'print $3'} | uniq`; do ps aux | grep $i | grep -v grep;done | grep -i ssh)
 if [ "$check_ipcs_lk" ];
- then echo -e $red"Cmd4: IPCS Check failed.  Doesn't necessarily mean anything:\n"$clroff$check_ipcs_lk;
+ then echo -e "\n"$red"Cmd4: IPCS Check failed.  Doesn't necessarily mean anything:\n"$clroff$check_ipcs_lk;
  num_fails=$((num_fails+1))
 fi
 
@@ -68,8 +68,10 @@ if [ "$num_fails" -gt 0 ]; then
  for i in $(\ls /lib*/libkeyutils*); do
   cmd_3_chk=$(strings $i | egrep 'connect|socket|inet_ntoa|gethostbyname');
   if [ "$cmd_3_chk" ]; then
-      stat $i | grep -i change;
+   echo -e "\nChange times of the compromised files:"
+   stat $i | grep -i change;
   fi;
+  echo -e "\nTotal Number of checks failed: "$num_fails
  done
 fi
 
