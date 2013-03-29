c280a9b0adf14eeaa5f966f5a966a20f9e0b8f9b
Marco Ferrufino <cpanelmarco@yahoo.com>
2013-03-28 20:02:02 -0500

    trying to make it readeable by splitting lines up

--- spam_check.sh
+++ spam_check.sh
@@ -16,7 +16,20 @@ echo;
 for j in $doms; do
    dom=$j;
    echo -e "\n\n Count / Subjects for domain = $j:";
-   for i in `cat $temp_dir/cptemp_eximbp | grep -B1 $dom | awk '{print $3}'`; do exim -Mvh $i | grep Subject; done | sort | uniq -c | sort -n | tail; done | awk '{split($4,encdata,"?"); command = (" base64 -d;echo"); if ($0~/UTF-8\?B/) {print "      "$1,$2,$3; print encdata[4] | command; close(command);} else {print}} END {print}'
+   for i in `cat $temp_dir/cptemp_eximbp | grep -B1 $dom | awk '{print $3}'`; do
+       exim -Mvh $i | grep Subject; 
+   done | sort | uniq -c | sort -n | tail; 
+   done | awk '{
+       split($4,encdata,"?"); 
+       command = (" base64 -d;echo"); 
+       if ($0~/UTF-8\?B/) {
+           print "      "$1,$2,$3; 
+           print encdata[4] | command; 
+           close(command);
+           }
+       else {print}
+       }
+      END {print}'
 
 # Domains sending:
 declare -a sendingaddys=($(egrep "<" $temp_dir/cptemp_eximbp | awk '{print $4}' | sort | uniq -c | sort -n | sed 's/<>/no_address_in_logs/g' | tail -4));
@@ -27,3 +40,4 @@ bigsender=$(echo ${sendingaddys[@]} | awk '{print $NF}'); echo -e "So the big se
 # for i in $doms; do echo -n $i": "; grep $i /etc/localdomains; done
 
 echo; for j in $doms; do dom=$j; echo "Mails attempting to be sent to domain [$j], from:"; cat $temp_dir/cptemp_eximbp | grep -B1 $dom | egrep -v "\-\-|$dom" | awk '{print $4}' | sort | uniq -c | sort -n | tail -5; echo; done
+
