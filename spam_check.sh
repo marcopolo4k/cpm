384f72e0f881358f8a0b1cd88bfa7c73b49f15bf
Marco Ferrufino <cpanelmarco@yahoo.com>
2013-03-28 20:32:56 -0500

    fix grep

--- spam_check.sh
+++ spam_check.sh
@@ -11,7 +11,7 @@ exim -bp > $temp_dir/cptemp_eximbp
 
 echo -e "\nDomains stopping up the queue:"; 
 cat $temp_dir/cptemp_eximbp | exiqsumm | sort -n | tail -5;
-doms=`cat $temp_dir/cptemp_eximbp | exiqsumm | sort -n | egrep -v "---|TOTAL" | tail -5 | awk '{print $5}'`
+doms=$(cat $temp_dir/cptemp_eximbp | exiqsumm | sort -n | egrep -v "\-\-\-|TOTAL" | tail -5 | awk '{print $5}')
 echo; 
 for j in $doms; do
    dom=$j;
@@ -19,17 +19,17 @@ for j in $doms; do
    for i in `cat $temp_dir/cptemp_eximbp | grep -B1 $dom | awk '{print $3}'`; do
        exim -Mvh $i | grep Subject; 
    done | sort | uniq -c | sort -n | tail; 
-   done | awk '{
-       split($4,encdata,"?"); 
-       command = (" base64 -d;echo"); 
-       if ($0~/UTF-8\?B/) {
-           print "      "$1,$2,$3; 
-           print encdata[4] | command; 
-           close(command);
-           }
-       else {print}
-       }
-      END {print}'
+done | awk '{
+    split($4,encdata,"?"); 
+    command = (" base64 -d;echo"); 
+    if ($0~/UTF-8\?B/) {
+        print "      "$1,$2,$3; 
+        print encdata[4] | command; 
+        close(command);
+        }
+    else {print}
+    }
+    END {print}'
 
 # Domains sending:
 declare -a sendingaddys=($(egrep "<" $temp_dir/cptemp_eximbp | awk '{print $4}' | sort | uniq -c | sort -n | sed 's/<>/no_address_in_logs/g' | tail -4));
