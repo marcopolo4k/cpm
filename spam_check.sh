a957a719ee04ad7585880d840c68d0bef8ec8e24
Marco Ferrufino <cpanelmarco@yahoo.com>
2013-05-02 19:18:52 -0500

    moved everything into functions

--- spam_check.sh
+++ spam_check.sh
@@ -38,23 +38,22 @@ function get_temp_file_dir () {
 }
 
 function run_eximbp () {
-echo -e "\nNow, beginning to run the command 'exim -bp'.  If this takes an excruciatingly long time, you can cancel (control-c) it, and the script should still work on the part that has already been created, which will probably show you what you need to know anyway."
+ echo -e "\nNow, beginning to run the command 'exim -bp'.  If this takes an excruciatingly long time, you can cancel (control-c) it, and the script should still work on the part that has already been created, which will probably show you what you need to know anyway."
  exim -bp > $temp_dir/cptemp_eximbp
 }
-
-get_temp_file_dir
-run_eximbp
 
 
 #todo: put this in an awk printf statement, report if domain is local/remote at the end:
 # Are they local?
 # for i in $doms; do echo -n $i": "; grep $i /etc/localdomains; done
 
+function exiqsumm_to_get_top_domains () {
 echo -e "\nDomains stopping up the queue:"; 
 cat $temp_dir/cptemp_eximbp | exiqsumm | sort -n | tail -5;
 
 # Get domains from Exim queue
 doms=$(cat $temp_dir/cptemp_eximbp | exiqsumm | sort -n | egrep -v "\-\-\-|TOTAL|Domain" | tail -5 | awk '{print $5}')
+}
 
 function check_if_local () {
  echo -e "\nDomains from above that are local:"
@@ -66,36 +65,50 @@ function check_if_local () {
   fi
  done
 }
-check_if_local 
 
-for j in $doms; do
-   dom=$j;
-   echo -e "\n\n Count / Subjects for domain = $j:";
-   for i in `cat $temp_dir/cptemp_eximbp | grep -B1 $dom | awk '{print $3}'`; do
-       exim -Mvh $i | grep Subject; 
-   done | sort | uniq -c | sort -n | tail; 
-done | awk '{
-    split($4,encdata,"?"); 
-    command = (" base64 -d -i;echo"); 
-    if ($0~/(UTF|utf)-8\?(B|b)/) {
-        printf "      "$1" "$2"  "$3" "; 
-        print encdata[4] | command; 
-        close(command);
-        }
-    else {print}
-    }
-    END {printf "\n"}'
+function get_subjects_of_top_domains () {
+ for j in $doms; do
+    dom=$j;
+    echo -e "\n\n Count / Subjects for domain = $j:";
+    for i in `cat $temp_dir/cptemp_eximbp | grep -B1 $dom | awk '{print $3}'`; do
+        exim -Mvh $i | grep Subject; 
+    done | sort | uniq -c | sort -n | tail; 
+ done | awk '{
+     split($4,encdata,"?"); 
+     command = (" base64 -d -i;echo"); 
+     if ($0~/(UTF|utf)-8\?(B|b)/) {
+         printf "      "$1" "$2"  "$3" "; 
+         print encdata[4] | command; 
+         close(command);
+         }
+     else {print}
+     }
+     END {printf "\n"}'
+}
 
 # Domains sending:
-declare -a sendingaddys=($(egrep "<" $temp_dir/cptemp_eximbp | awk '{print $4}' | sort | uniq -c | sort -n | sed 's/<>/no_address_in_logs/g' | tail -4));
-echo -e "\nAddresses sending out: " ${sendingaddys[@]} "\n"| sed 's/ \([0-9]*\) /\n\1 /g'
-bigsender=$(echo ${sendingaddys[@]} | awk '{print $NF}'); 
-echo -e "So the big sender is:\n"$bigsender
+function find_addresses_sending_out () {
+ declare -a sendingaddys=($(egrep "<" $temp_dir/cptemp_eximbp | awk '{print $4}' | sort | uniq -c | sort -n | sed 's/<>/no_address_in_logs/g' | tail -4));
+ echo -e "\nAddresses sending out: " ${sendingaddys[@]} "\n"| sed 's/ \([0-9]*\) /\n\1 /g'
+ bigsender=$(echo ${sendingaddys[@]} | awk '{print $NF}'); 
+ echo -e "So the big sender is:\n"$bigsender
+}
 
-echo; 
-for j in $doms; do
-    dom=$j; 
-    echo "Mails attempting to be sent to domain [$j], from:"; 
-    cat $temp_dir/cptemp_eximbp | grep -B1 $dom | egrep -v "\-\-|$dom" | awk '{print $4}' | sort | uniq -c | sort -n | tail -5; 
-    echo; 
-done
+function find_addresses_sending_to_top_domains () {
+ echo; 
+ for j in $doms; do
+     dom=$j; 
+     echo "Mails attempting to be sent to domain [$j], from:"; 
+     cat $temp_dir/cptemp_eximbp | grep -B1 $dom | egrep -v "\-\-|$dom" | awk '{print $4}' | sort | uniq -c | sort -n | tail -5; 
+     echo; 
+ done
+}
+
+# Run all functions
+get_temp_file_dir
+run_eximbp
+exiqsumm_to_get_top_domains 
+check_if_local 
+get_subjects_of_top_domains 
+find_addresses_sending_out
+find_addresses_sending_to_top_domains
