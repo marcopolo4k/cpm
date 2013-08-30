5f9bb80806d0773a1fef99e779d4c31b6f12755c
Marco Ferrufino <cpanelmarco@yahoo.com>
2013-08-30 17:17:30 -0500

    Reformatted everything Colors based off BOates' old webstats probe really

--- backup_report.sh
+++ backup_report.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Quick backup report script
+# Quick backup report script written by: Marco Ferrufino
 #
 # Description: https://staffwiki.cpanel.net/bin/view/LinuxSupport/CPanelBackups
 #
@@ -10,13 +10,15 @@
 
 backlogdir=/usr/local/cpanel/logs/cpbackup;
 
+
 # check if new backups are enabled
 function check_new_backups() {
+ echo -e "\n\n\033[36m[ cPTech Backup Report v1.0 ]\033[0m\n";
  new_enabled=$(grep BACKUPENABLE /var/cpanel/backups/config 2>/dev/null | awk -F"'" '{print $2}')
  if [ "$new_enabled" = "yes" ]; then new_status='\033[1;32m'Enabled'\033[0m'
  else new_status='\033[1;31m'Disabled'\033[0m'
  fi
- echo -e "\nNew Backups = $new_status"
+ echo -e "New Backups = $new_status"
 }
 
 # check if legacy or new backups are enabled.  if each one is, then show how many users are skipped
@@ -30,7 +32,7 @@ function check_legacy_backups() {
 
 # look at start, end times.  print number of users where backup was attempted
 function print_start_end_times () {
-echo -e "\nCurrent Backup Logs in "$backlogdir":\n"
+echo -e "\n\n\033[36m[ Current Backup Logs in "$backlogdir" ]\033[0m\n";
 if [ -e $backlogdir ]; then
  cd $backlogdir;
  for i in `\ls`; do
@@ -38,17 +40,25 @@ if [ -e $backlogdir ]; then
   \ls -lrth | grep $i | awk '{print $6" "$7" "$8}';
   echo -ne " Number of users backed up:\t";  grep "user :" $i | wc -l;
  done;
- echo -e "\nTotal users:"; wc -l /etc/trueuserdomains;
+ echo -e "\n\n\033[36m[ Expected Number of Users ]\033[0m\n";
+ wc -l /etc/trueuserdomains;
 fi;
+}
+
+function exceptions_heading() {
+ echo -e "\n\n\033[36m[ A count of user exceptions ]\033[0m\n";
 }
 
 function list_legacy_exceptions() {
 legacy_users=$(grep "LEGACY_BACKUP=1" /var/cpanel/users/* | wc -l);
 if [ $legacy_enabled == "yes" ]; then
- echo -e "\nLegacy Backups Exceptions";
  oldxs=$(egrep "LEGACY_BACKUP=0" /var/cpanel/users/* | wc -l);
- if [ $oldxs -gt 0 ]; then echo "Number of real Legacy backup exceptions: "$oldxs; fi;
  skip_file_ct=$(wc -l /etc/cpbackup-userskip.conf 2>/dev/null)
+ if [ $oldxs -gt 0 -o "$skip_file_ct" ]; then
+  echo "oldxs="$oldxs " skip file="$skip_file_ct
+  echo -e "Legacy Backups Exceptions";
+ fi
+ if [ $oldxs -gt 0 ]; then echo "Number of real Legacy backup exceptions: "$oldxs; fi;
  if [ -n "$skip_file_ct" ]; then echo -e "\nExtra Information: This skip file should no longer be used"$skip_file_ct; fi
 elif [ $legacy_users -gt 0 -a $legacy_status == "Disabled" ]; then
  echo -e "\nExtra Information: Legacy Backups aren't enabled, but there are $legacy_users users ready to use them."
@@ -65,7 +75,7 @@ fi
 }
 
 function count_local_new_backups() {
-echo -e "\n\nA count of the backup files on local disk currently:"
+echo -e "\n\n\033[36m[ A count of the backup files on local disk currently ]\033[0m\n";
 new_backup_dir=$(awk '/BACKUPDIR/ {print $2}' /var/cpanel/backups/config 2>/dev/null)
 if [ -n "$new_backup_dir" ]; then
  number_new_backups=$(\ls $new_backup_dir/*/accounts/ 2>/dev/null | wc -l)
@@ -87,6 +97,7 @@ done
 check_new_backups
 check_legacy_backups
 print_start_end_times 
+exceptions_heading
 list_legacy_exceptions
 list_new_exceptions
 count_local_new_backups
