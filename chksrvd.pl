57945750ef88ece20f0e0fd68a15fef50774e8ef
Marco Ferrufino <cpanelmarco@yahoo.com>
2013-09-04 23:10:09 -0500

    1st working version

---
+++ chksrvd.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+#1.) open log file
+open my $file, 'chkservd.log' or die "couldn't open file $!";
+
+
+#2.) make a for loop that reads the file
+my $lastdate="";
+foreach my $line (readline $file) {
+
+#3.) no longer plan to break file into hash of hashes called "service_status"
+
+# Set the date
+$lastdate = $1 if $line =~ /(\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2} \+[0-9]{4}\]).*/ ;
+#DEBUG
+#print $lastdate ;
+
+# getting the lines using the old regex
+    if ($line !~ /Restarting|nable|\*\*|imeout|ailure|terrupt|100%|9[89]%|second/ && $line =~ /:\-\]/){
+            print $lastdate, " ....\n";
+        }
+    if ($line =~ /Restarting|nable|\*\*|imeout|ailure|terrupt|100%|9[89]%|second/){
+        @a1 = split /(\.){2,}/,$line;
+        foreach (@a1) {
+            if (/:\-\]/) {
+                print $lastdate, " ", $_, "\n";
+            }
+        }
+        #DEBUG print "\nFOR LOOP DONE\n";
+    }
+}
+
+close $file;
