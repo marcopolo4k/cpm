84a3e0e43453a645e87570c8db40a1a8a2a9e9b4
Marco Ferrufino <cpanelmarco@yahoo.com>
2013-09-06 08:54:36 -0500

    added non-match errors

--- chksrvd.pl
+++ chksrvd.pl
@@ -1,33 +1,37 @@
 #!/usr/bin/perl
 use strict;
+my $debug=1;
 
-#1.) open log file
+# Open log file
 open my $file, '/var/log/chkservd.log' or die "couldn't open file $!";
 
 
-#2.) make a for loop that reads the file
+# For loop reads the file
 my $lastdate="";
 foreach my $line (readline $file) {
 
-#3.) no longer plan to break file into hash of hashes called "service_status"
+    # Set the date
+    $lastdate = $1 if $line =~ /(\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2} [+-][0-9]{4}\]).*/ ;
+    #if ( $debug == 1 ){ print $lastdate; }
 
-# Set the date
-$lastdate = $1 if $line =~ /(\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2} [+-][0-9]{4}\]).*/ ;
-#DEBUG
-#print $lastdate ;
-
-# getting the lines using the old regex
+    # These are usually trash lines
     if ($line !~ /Restarting|nable|\*\*|imeout|ailure|terrupt|100%|9[89]%|second/ && $line =~ /:\-\]/){
             print $lastdate, " ....\n";
         }
+    # Main search
     if ($line =~ /Restarting|nable|\*\*|imeout|ailure|terrupt|100%|9[89]%|second/){
         my @array_fields = split /(\.){2,}/,$line;
-        foreach (@array_fields) {
-            if (/:\-\]/) {
-                print $lastdate, " ", $_, "\n";
+        if (scalar(@array_fields) > 1){
+            #if ( $debug == 1 ){ print scalar(@array_fields),"\n"; }
+            foreach (@array_fields) {
+                if (/:\-\]/) {
+                    print $lastdate, " ", $_, "\n";
+                }
+            } 
+        } else { 
+                print $lastdate, " ", $line;
             }
-        }
-        #DEBUG print "\nFOR LOOP DONE\n";
+        #if ( $debug == 1 ){ print "\nFOR LOOP DONE\n"; }
     }
 }
 
