573b716726d66cc876905ad5824b1b1617d54178
Marco Ferrufino <cpanelmarco@yahoo.com>
2013-10-07 16:14:45 -0500

    version 0.2: add options

--- local_site_check.sh
+++ local_site_check.sh
@@ -10,38 +10,121 @@
 # This script checks if locally-hosted websites are responding via Apache, and creates a summary_file at:
 # /root/site_summary.HOST_IP_ADDRESS.cP.DATE_TAG
 
-# Version: 0.1
+# Version: 0.2
 
 # Feel free to customize the following fields:
 tmp_dir=/root/cptmp.doms
-host_backup_file=/etc/hosts.cppremig.bk.$(date +%Y%m%d).$(date +%H).$(date +%M)
 summary_file=/root/site_summary.$(hostname -i).cP.$(date +%Y%m%d).$(date +%H).$(date +%M)
 
-# Backup hosts file
-cp -pv /etc/hosts $host_backup_file
+function debug() {
+ debug="on"
+ if [ "$debug" = "on" ]; then
+  echo $1
+ fi
+}
 
-# Add files into /etc/hosts to ensure we only look at the locally hosted versions of the websites:
-for i in $(cut -d: -f1 /etc/trueuserdomains); do echo -e "127.0.0.1\t\t$i" >> /etc/hosts; done
+print_help(){
+    echo 'usage:'
+    echo './local_site_check.sh <detl>'
+    echo
+    echo 'optional:'
+    echo '-d use [D]NS resolution to get site content'
+    echo '-e use local resolution only (place 127.0.0.1 in /[E]tc/hosts for [E]ach domain) (this is default)'
+    echo '-t use /etc/[T]rueuserdomains (necessary for migrations) (this is default)'
+    echo '-l use /etc/[L]ocaldomains (can be done with cPanel servers)'
+    echo; echo; exit 1
+}
 
-# Main work
-mkdir $tmp_dir;
-for i in $(cut -d: -f1 /etc/trueuserdomains); do
-  echo $i;
-  curl $i | head -100 | lynx -stdin -dump | awk NF | head > $tmp_dir/$i;
-done;
-for i in $(\ls -A $tmp_dir/); do
-  echo $i: ;
-  cat $tmp_dir/$i ;
-  echo;echo "============================";
-done > $summary_file
+# Set default options
+dns_resolution="0"
+local_resolution="1"
+use_trueuserdomains="1"
+use_localdomains="0"
 
-# Cleanup
-cp -pv $host_backup_file /etc/hosts
+# Get options
+while getopts ":detl" opt; do
+    case $opt in
+        d) dns_resolution="1";;
+        e) local_resolution="1";;
+        t) use_trueuserdomains="1";;
+        l) use_localdomains="1";;
+        h) print_help;;
+        \?) echo "invalid option: -$OPTARG"; echo; print_help;;
+        :) echo "option -$OPTARG requires an argument."; echo; print_help;;
+    esac
+done
 
-if [ -d $tmp_dir ]; then
- rm -rvf $tmp_dir/*; rmdir -v $tmp_dir
-else echo "Error: "$tmp_dir" doesn't exist."
+## Get options, but with getopt
+# args=`getopt -l help :detl: $*`
+# for i in $args; do
+#     case $i in
+#     -d) echo "-p"
+#         ;;
+#     -e) shift;
+#         optarg=$1;
+#         echo "-q $optarg"
+#         ;;
+#     --help)
+#         echo "--help"
+#         ;;
+#     esac
+# done
+
+if [[ $# -eq 0 || -z $sourceserver ]]; then print_help; fi  # check for existence of required var
+
+
+main (){
+    debug "Now in main.  domain_list is ${domain_list}"
+    mkdir $tmp_dir;
+    for i in $domain_list; do
+        echo $i;
+        curl $i | head -100 | lynx -stdin -dump | awk NF | head > $tmp_dir/$i;
+    done;
+    for i in $(\ls -A $tmp_dir/); do
+        echo $i: ;
+        cat $tmp_dir/$i ;
+        echo;echo "============================";
+    done > $summary_file
+
+
+    if [ -d $tmp_dir ]; then
+        rm -rvf $tmp_dir/*; rmdir -v $tmp_dir
+    else echo "Error: "$tmp_dir" doesn't exist."
+    fi
+}
+
+if [[ $use_trueuserdomains == "1" ]]; then
+    domain_list=$(cut -d: -f1 /etc/trueuserdomains)
+    debug "domain_list is ${domain_list}"
 fi
+
+if [[ $use_localdomains == "1" ]]; then
+    domain_list=$(cat /etc/localdomains)
+    debug "domain_list is ${domain_list}"
+fi
+
+if [[ $local_resolution == "1" ]]; then
+
+    # Backup hosts file
+    host_backup_file=/etc/hosts.cppremig.bk.$(date +%Y%m%d).$(date +%H).$(date +%M)
+    cp -pv /etc/hosts $host_backup_file
+
+    # Add files into /etc/hosts to ensure we only look at the locally hosted versions of the websites:
+    for i in $(cut -d: -f1 /etc/trueuserdomains); do echo -e "127.0.0.1\t\t$i" >> /etc/hosts; done
+
+    main
+
+    # Cleanup
+    cp -pv $host_backup_file /etc/hosts
+
+fi
+
+
+if [[ $dns_resolution == "1" ]]; then
+    main
+fi
+
+
 
 # All done
 echo -e "\nSite Check Complete.  Summary at:\n"$summary_file"\n"
