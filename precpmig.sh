7d2a0e7c98ac663cdef9d652fde37064ebb4d7da
root <root@cent58-x64.113990325.marco.cpanel.net>
2013-10-14 20:56:11 -0500

    added & fixed libkey check for basic functionality

--- precpmig.sh
+++ precpmig.sh
@@ -13,7 +13,7 @@ scripthome="/root/.cppremig"
 #############################################
 
 debug() {
- debug="on"
+ debug="off"
  if [ "$debug" = "on" ]; then
   echo -e $1
  fi
@@ -92,20 +92,21 @@ set_logging_mode(){
 # Libkey Check
 lc_checkfor() {
 	if [ "$1" ];
-		then echo -e "$2\n$1\n$3" &> >(tee --append $logfile);
+		then echo -e "$2\n$1\n$3" >> $logfile
 		num_fails=$((num_fails+1))
-		else echo "Passed." &> >(tee --append $logfile)
+		else echo "Passed." >> $logfile
 	fi
 }
 
 lc_print_header() {
 	echo -e "\nSearching for Libkey compromise. The '6 commands' are described here:\n
-http://docs.cpanel.net/twiki/bin/view/AllDocumentation/CompSystem" &> >(tee --append $logfile)
+http://docs.cpanel.net/twiki/bin/view/AllDocumentation/CompSystem" >> $logfile
+lc_num_fails=0
 }
 
 # Libkey Check: These general checks are not the 6 commands listed on the website
 lc_general_checks() {
-	echo -e "\nFirst general checks:"  &> >(tee --append $logfile)
+	echo -e "\nFirst general checks:"  >> $logfile
 	libkey_ver_check=$(\ls -la $(ldd $(which sshd) |grep libkey | cut -d" " -f3))
 	#length_check $libkey_ver_check
 	libkey_check_results=$(echo $libkey_ver_check | egrep "1.9|1.3.2|1.3.0|1.2.so.2|1.2.so.0")
@@ -118,83 +119,86 @@ lc_general_checks() {
 
 	assiciated_rpm_check=$(echo $assiciated_rpm | grep "is not owned by any package")
 	if [ "$assiciated_rpm_check" ]; then
-		echo -e "libkey check failed due to associated RPM:\n"$assiciated_rpm &> >(tee --append $logfile)
+		echo -e "libkey check failed due to associated RPM:\n"$assiciated_rpm >> $logfile
 		lc_num_fails=$((lc_num_fails+1))
 	else
-		echo -e "RPM associated with libkey file:\n"$assiciated_rpm &> >(tee --append $logfile)
-		echo "Passed." &> >(tee --append $logfile)
+		echo -e "RPM associated with libkey file:\n"$assiciated_rpm >> $logfile
+		echo "Passed." >> $logfile
 	fi
 }
 
 # Libkey Check: Here the 6 commands listed on the website 
 lc_command_1() {
-	echo -e "\nCommand 1 Test:" &> >(tee --append $logfile)
+	echo -e "\nCommand 1 Test:" >> $logfile
 	keyu_pckg_chg_test=$(rpm -V keyutils-libs)
 	lc_checkfor "$keyu_pckg_chg_test" "keyutils-libs check failed. The rpm shows the following file changes: " "\n If the above changes are any of the 
  following, then maybe it's ok (probable false positive - you could ask the sysadmin what actions may have caused these):
  .M.....T
  However, if changes are any of the following, then it's definitely suspicious:
  S.5.L...
- see 'man rpm' and look for 'Each of the 8 characters'" &> >(tee --append $logfile)
+ see 'man rpm' and look for 'Each of the 8 characters'" >> $logfile
 }
 
 lc_command_2() {
-	echo -e "\nCommand 2 Test:" &> >(tee --append $logfile)
+	echo -e "\nCommand 2 Test:" >> $logfile
 	cmd_2_chk=$(\ls -la $thelibkey | egrep "so.1.9|so.1.3.2|1.2.so.2");
 	lc_checkfor "$cmd_2_chk" "Known bad package check failed. The following file is linked to libkeyutils.so.1: "
 }
 
 lc_command_3() {
-	echo -e "\nCommand 3 Test:" &> >(tee --append $logfile)
+	echo -e "\nCommand 3 Test:" >> $logfile
 	cmd_3_chk=$(strings $thelibkey | egrep 'connect|socket|inet_ntoa|gethostbyname')
 	lc_checkfor "$cmd_3_chk" "libkeyutils libraries contain networking tools: "
 }
 
 lc_command_4() {
-	echo -e "\nCommand 4 Test:" &> >(tee --append $logfile)
+	echo -e "\nCommand 4 Test:" >> $logfile
 	check_ipcs_lk=$(for i in `ipcs -mp | grep -v cpid | awk {'print $3'} | uniq`; do ps aux | grep '\b'$i'\b' | grep -v grep;done | grep -i ssh)
 	lc_checkfor "$check_ipcs_lk" "IPCS Check failed.  This is sometimes a false positive:"
 }
 
 lc_command_5() {
-	echo -e "\nCommand 5 Test is not designed to run by this automated script" &> >(tee --append $logfile)
+	echo -e "\nCommand 5 Test is not designed to run by this automated script" >> $logfile
 }
 
 lc_command_6() {
-	echo -e "\nCommand 6 Test:" &> >(tee --append $logfile)
+	echo -e "\nCommand 6 Test:" >> $logfile
 	cmd6fail=0
 	for i in $(ldd /usr/sbin/sshd | cut -d" " -f3); do
 		sshd_library=$(rpm -qf $i);
 		if [ ! "sshd_library" ]; then
-			echo -e "\n"$i" has no associated library." &> >(tee --append $logfile); echo $sshd_library &> >(tee --append $logfile);
+			echo -e "\n"$i" has no associated library." >> $logfile;
+            echo $sshd_library >> $logfile
 			cmd6fail=$((cmd6fail+1))
 		fi;
 	done
 	if [ "$cmd6fail" -gt 0 ]; then
 		lc_num_fails=$((lc_num_fails+1))
-	else echo "Passed." &> >(tee --append $logfile)
+	else echo "Passed." >> $logfile
 	fi
 }
 
 
 # Libkey Check: Print Summary and show the dates on the files
 lc_summary() {
+    debug "lc_num_fails is $lc_num_fails"
 	if [ "$lc_num_fails" -gt 0 ]; then
-		echo -e "\nPossible change times of the compromised files:" &> >(tee --append $logfile)
+		echo -e "\nPossible change times of the compromised files:" >> $logfile
 		for i in $(\ls /lib*/libkeyutils*); do
 			cmd_3_chk=$(strings $i | egrep 'connect|socket|inet_ntoa|gethostbyname');
 			if [ "$cmd_3_chk" ]; then
-				stat $i | grep -i change &> >(tee --append $logfile);
+				stat $i | grep -i change >> $logfile
 			fi;
 		done
 		echo -e "\nTotal Number of checks failed: "$lc_num_fails" (out of 7 checks currently)\n\n
 The following is a general guide to interpret results:
   1 check failed = probably false positive. This is usually commands 1, 4, or 6
   2 checks failed = somewhat likely real
-  3+ checks failed = definitely real" &> >(tee --append $logfile)
+  3+ checks failed = definitely real" >> $logfile
+    exit 0
 	fi
  
-	echo &> >(tee --append $logfile)
+	echo >> $logfile
 }
 
 setup_remote(){
@@ -303,7 +307,7 @@ error_check(){
     ###################
     #Critical checks
     ####################
-    criticals="`echo \"$logcheck\" | egrep "putsomethinghere"`"
+    criticals="`echo \"$logcheck\" | egrep "check failed"`"
     if [[ ! $criticals == "" ]]; then
         echo -en "\E[30;41m Critical error(s) detected!\E[0m \n"
         echo "######!!!!! Critical error(s) detected! !!!!!#####" >> $logfile
@@ -432,17 +436,6 @@ fi
 # print into
 print_intro
 
-# libkey check
-lc_print_header
-lc_general_checks
-lc_command_1
-lc_command_2
-lc_command_3
-lc_command_4
-lc_command_5
-lc_command_6
-lc_summary
-error_check
 
 # install sshpass
 if [ ! -f $scripthome/.sshpass/sshpass-1.05/sshpass ]; then
@@ -468,6 +461,18 @@ epoch=`date +%s`
 
 # Set logging mode
 set_logging_mode
+
+# libkey check
+lc_print_header
+lc_general_checks
+lc_command_1
+lc_command_2
+lc_command_3
+lc_command_4
+lc_command_5
+lc_command_6
+lc_summary
+error_check
 
 # Setup Remote Server
 if [[ $skipremotesetup == "1" ]]; then
