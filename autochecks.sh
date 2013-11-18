e5811968882794aff385f529d513d8bf3e7627d2
Marco Ferrufino <cpanelmarco@yahoo.com>
2013-11-18 08:56:49 -0600

    improve chksrvd, add assp check

--- autochecks.sh
+++ autochecks.sh
@@ -95,7 +95,7 @@ checkfor "$hta" "Global? htaccess file in home" "This is sometimes associated wi
 # https://staffwiki.cpanel.net/LinuxSupport/OneLiners#Show_chksrvd_failures
 echo -e "\nRecent chksrvd errors:";
 echo -n "starting search at: ";  tail -3200 /var/log/chkservd.log | awk '{if ($1~/\[20/) print $1,$2,$3}' | head -1
-every_n_min=10; tail -3200 /var/log/chkservd.log |awk -v n=$every_n_min '{if ($1~/\[20/) lastdate=$1" "$2" "$3; split($2,curdate,":"); dmin=(curdate[2]-lastmin); dhr=(curdate[1]-lasthr); if ($0!~/Restarting|nable|\*\*|imeout|ailure|terrupt/ && $0~/:-]/) print lastdate"....."; for (i=1;i<=NF;i=i+1) if ($i~/Restarting|nable|\*\*|imeout|ailure|terrupt|100%|9[89]%|second/) if ($1~/\[20/) print $1,$2,$3,$(i-1),$i,$(i+1); else print lastdate,$(i-1),$i,$(i+1); if($1~/\[20/ && (lastmin!=0 || lasthr!=0) && (dmin>n || (dhr==1 && (dmin>-(60-n))) || dhr>1 )) print $1,$2,$3" check took longer than "n" minutes. (hr:min): "dhr":"dmin; if ($1~/\[20/) {lastmin=curdate[2]; lasthr=curdate[1]} }'
+every_n_min=10; tail -3200 /var/log/chkservd.log |awk -v n=$every_n_min '{if ($1~/\[20/) lastdate=$1" "$2" "$3; split($2,curdate,":"); dmin=(curdate[2]-lastmin); dhr=(curdate[1]-lasthr); if ($0!~/Restarting|nable|\*\*|imeout|ailure|terrupt/ && $0~/:-]/) print lastdate"....."; for (i=1;i<=NF;i=i+1) if ($i~/Restarting|nable|\*\*|imeout|ailure|terrupt|100%|9[89]%|second/) if ($1~/\[20/) print $1,$2,$3,$(i-1),$i,$(i+1); else print lastdate,$(i-1),$i,$(i+1); if($1~/\[20/ && (lastmin!=0 || lasthr!=0) && (dmin>n || (dhr==1 && (dmin>-(60-n))) || dhr>1 )) print $1,$2,$3" check took longer than "n" minutes. (hr:min): "dhr":"dmin; if ($1~/\[20/) {lastmin=curdate[2]; lasthr=curdate[1]} }' | { head -3; echo -e "\n...\n"; tail;}
 echo "Current time: "; date
 
 # this section is repeated in rc.remote.  #todo: fix that.
@@ -214,3 +214,6 @@ fi
 
 #ac3524=$(fgrep -r open_tty /usr/local/apache/)
 #checkfor "$ac3524" "see ac3524:"
+
+hasassp=$(ps aux | grep assp)
+checkfor "$hasassp" "ASSP found:"
