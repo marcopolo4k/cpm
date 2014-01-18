b4b8cd727db88031941afd3484d29259fb8b2eb3
Marco Ferrufino <cpanelmarco@yahoo.com>
2014-01-17 18:18:33 -0600

    cleaning, cleaning

--- chksrvd-timepiece.pl
+++ chksrvd-timepiece.pl
@@ -6,11 +6,10 @@ use Time::Seconds;
 #use Date::Calc;
 
 #Todo:
-# - better method of time math
 # - allow for options (only last x lines, or y hours)
 
 sub debug {
-    my $debug_toggle = "yes";
+    my $debug_toggle = "no";
     if ($debug_toggle eq "yes") {
         print "(debug) @_\n";
     }
@@ -19,13 +18,12 @@ sub debug {
 # Variables
 my $line;
 my $lastdate;
-my @curdate;
-my $diffofdate;
+my $curdate;
+my $duration;
+my $duration_min;
 my $lasthr;
 my $lastmin;
 my $line_has_date=0;
-my $dhr;
-my $dmin;
 my $every_n_min;
 chomp(my $every_n_sec=`grep chkservd_check_interval /var/cpanel/cpanel.config | cut -d= -f2`);
 
@@ -45,26 +43,18 @@ if ( $every_n_sec < 1 ) {
 # Open log file
 open my $file, '/var/log/chkservd.log' or die "couldn't open file $!";
 
-# For loop reads the file
-#foreach $line (readline $file) {
+# While loop reads the file
 while ($line = <$file>) {
-    #&debug($line);
     # Set the date
     if ($line =~ /\[(\d{4}(-\d{2}){2} \d{2}(:\d{2}){2} [+-]\d{4})\].*/) {
-    #if ($_ =~ /\[(\d{4}(-\d{2}){2} \d{2}(:\d{2}){2} [+-]\d{4})\].*/) {
         $line_has_date = 1;
         &debug("one is $1");
 
         ##########
         # Time::Piece
-        my $for_time_piece;
-        my $curdate = Time::Piece->strptime($1, "%Y-%m-%d %H:%M:%S %z");
+        $curdate = Time::Piece->strptime($1, "%Y-%m-%d %H:%M:%S %z");
         &debug("curdate is now $curdate");
         #get rid of this: my $curdate_minus1 = ($curdate - ONE_DAY);
-        my $curdate_hr = ($curdate->hour);
-        &debug("curdate_hr is $curdate_hr");
-        my $curdate_min = ($curdate->min);
-        &debug("curdate_min is $curdate_min");
 
         # Calculate time difference between this & last check, in minutes & hours
         # If this is the first time run, establish the starting values
@@ -74,10 +64,12 @@ while ($line = <$file>) {
             &debug ("after setting first occurence, lastdate is ", $lastdate, "\n");
         }
         else {
-            $diffofdate = $curdate - $lastdate;
-            &debug("diffofdate is $diffofdate");
-            &debug ("diffofdate is ", $diffofdate->minutes, " minutes");
-            &debug ("diffofdate is ", $diffofdate->hours, " hours");
+            $duration = $curdate - $lastdate;
+            &debug("duration is $duration");
+            &debug ("duration is ", $duration->minutes, " minutes");
+            &debug ("duration is ", $duration->hours, " hours");
+            $duration_min=$duration->minutes;
+            &debug ("duration_min is ", $duration_min);
         }
     }
 
@@ -89,7 +81,6 @@ while ($line = <$file>) {
     if ($line =~ /Restarting|nable|\*\*|imeout|ailure|terrupt|100%|9[89]%|second/){
         my @array_fields = split /(\.){2,}/,$line;
         if (scalar(@array_fields) > 1){
-            #&debug(@array_fields);
             foreach (@array_fields) {
                 if (/:\-\]/) {
                     print "[", $lastdate, "] ", $_, "\n";
@@ -98,22 +89,23 @@ while ($line = <$file>) {
         } else {
                 print "[", $lastdate, "] ", $line;
             }
-        #&debug("\nFOR LOOP DONE\n");
+        #&debug("\nWHILE LOOP DONE\n");
     }
 
-    # If this line includes a date, either one is true:
-    # difference in minutes > limit
-    # difference in hours is == 1, AND check that difference in minutes is not too negative
-    # difference in hours is > 1
-    &debug("line_has_date is: $line_has_date\nlastmin is: $lastmin\nlasthr is: $lasthr\ndmin is: $dmin\nevery_n_min is: $every_n_min\ndhr is: $dhr\ndmin is: $dmin\nevery_n_min is: $every_n_min");
-    if ($line_has_date && ($lastmin!=0 || $lasthr!=0) && ($dmin>$every_n_min || ($dhr==1 && ($dmin>(-1*(60-$every_n_min)))) || $dhr>1 )) {
-        print "$lastdate Check took longer than $every_n_min minutes. (hr:min): $dhr:$dmin\n";
+    #old:
+#&debug("line_has_date is: $line_has_date\nlastmin is: $lastmin\nlasthr is: $lasthr\ndmin is: $dmin\nevery_n_min is: $every_n_min\ndhr is: $dhr\ndmin is: $dmin\nevery_n_min is: $every_n_min");
+    #if ($line_has_date && ($lastmin!=0 || $lasthr!=0) && ($dmin>$every_n_min || ($dhr==1 && ($dmin>(-1*(60-$every_n_min)))) || $dhr>1 )) {
+    #    print "$lastdate Check took longer than $every_n_min minutes. (hr:min): $dhr:$dmin\n";
+    #}
+    &debug ("duration_min is ", $duration_min);
+    if($duration_min > $every_n_min) {
+        printf "[$lastdate] Last check took %.0f minutes\n", $duration_min;
     }
 
     # Set lastdate for next round
-    #if ($line_has_date) {
-    #    $lastdate=$curdate;
-    #}
+    if ($line_has_date) {
+        $lastdate=$curdate;
+    }
 
 }
 
