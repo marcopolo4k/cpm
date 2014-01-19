9bcd93ce06335fc36014a6f4e81e78173fd90b89
Marco Ferrufino <cpanelmarco@yahoo.com>
2014-01-18 21:12:46 -0600

    now it tails the argument # of days

--- chksrvd-timepiece.pl
+++ chksrvd-timepiece.pl
@@ -1,22 +1,28 @@
 #!/usr/local/cpanel/3rdparty/perl/514/bin/perl
 
 use strict;
+#use warnings;
 use Time::Piece;
 use Time::Seconds;
-#use Date::Calc;
+use File::ReadBackwards;
 
 #Todo:
-# - allow for options (only last x lines, or y hours)
+# make the input days calculation accurate
 
 sub debug {
     my $debug_toggle = "no";
-    if ($debug_toggle eq "yes") {
-        print "(debug) @_\n";
+    if(defined $debug_toggle){
+        if ($debug_toggle eq "yes") {
+            if($_[1]){
+                print "(debug) @_\n";
+            }
+        }
     }
 }
 
 # Variables
 my $line;
+my @lines;
 my $lastdate;
 my $curdate;
 my $duration;
@@ -26,6 +32,7 @@ my $lastmin;
 my $line_has_date=0;
 my $every_n_min;
 chomp(my $every_n_sec=`grep chkservd_check_interval /var/cpanel/cpanel.config | cut -d= -f2`);
+my $file  = '/var/log/chkservd.log';
 
 # Set search time for 'system too slow' check
 # IDK why this didn't work:
@@ -40,11 +47,33 @@ if ( $every_n_sec < 1 ) {
     &debug("every_n_min is: $every_n_min");
 }
 
-# Open log file
-open my $file, '/var/log/chkservd.log' or die "couldn't open file $!";
+## Open log file
+# old way:
+#open my $file, '/var/log/chkservd.log' or die "couldn't open file $!";
+#my $limit = shift or die "Number of lines not specified, try 3200 (for ~one day).\n";
+my $days = shift or die "Please enter number of previous days (estimate) as an argument.\n";
+# this is a guessed average 12*24*6.5 (#lines per check seem to be 5-8)
+my $limit = ($days*1872);
+
+# Tail the file (opeing the whole thing is ridonculous time-wise)
+sub reverse_lines {
+    my $lim = $limit;
+    my $bw = File::ReadBackwards->new( $file ) or die "can't read $file: $!\n" ;
+
+    my $line;
+    my @lines;
+    while( defined( my $line = $bw->readline ) ) {
+        push @lines, $line;
+        last if --$lim <= 0;
+    }
+    reverse @lines;
+}
+
+@lines = &reverse_lines();
 
 # While loop reads the file
-while ($line = <$file>) {
+#while ($line = <$file>) {
+foreach my $line(@lines) {
     # Set the date
     if ($line =~ /\[(\d{4}(-\d{2}){2} \d{2}(:\d{2}){2} [+-]\d{4})\].*/) {
         $line_has_date = 1;
@@ -92,14 +121,11 @@ while ($line = <$file>) {
         #&debug("\nWHILE LOOP DONE\n");
     }
 
-    #old:
-#&debug("line_has_date is: $line_has_date\nlastmin is: $lastmin\nlasthr is: $lasthr\ndmin is: $dmin\nevery_n_min is: $every_n_min\ndhr is: $dhr\ndmin is: $dmin\nevery_n_min is: $every_n_min");
-    #if ($line_has_date && ($lastmin!=0 || $lasthr!=0) && ($dmin>$every_n_min || ($dhr==1 && ($dmin>(-1*(60-$every_n_min)))) || $dhr>1 )) {
-    #    print "$lastdate Check took longer than $every_n_min minutes. (hr:min): $dhr:$dmin\n";
-    #}
     &debug ("duration_min is ", $duration_min);
-    if($duration_min > $every_n_min) {
-        printf "[$lastdate] %.0f minutes since last check\n", $duration_min;
+    if(defined $duration_min){
+        if($duration_min > $every_n_min) {
+            printf "[$lastdate] %.0f minutes since last check\n", $duration_min;
+        }
     }
 
     # Set lastdate for next round
@@ -108,4 +134,3 @@ while ($line = <$file>) {
     }
 
 }
-
