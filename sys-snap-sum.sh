def8aad802e0ecd8c8236e65d450ee102fa3cf59
Marco Ferrufino <marco@cpanel.net>
2014-03-17 17:44:28 -0500

    wording

--- sys-snap-sum.sh
+++ sys-snap-sum.sh
@@ -220,7 +220,7 @@ done
 
 # The MIT License (MIT)
 # 
-# Copyright (c) <year> <copyright holders>
+# Copyright (c) 2014 Marco Ferrufino
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
