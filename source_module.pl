07fb99c30533b5aef935529316914509f2b5d188
Marco <polo177@gmail.com>
2014-08-01 15:01:23 -0500

    make room for printing message if empty need to fix printing messages

--- source_module.pl
+++ source_module.pl
@@ -5,10 +5,11 @@ use warnings;
 my $filename = $ARGV[0];
 my $function_name = $ARGV[1];
 my @file_modules;
+my %found;
 
 open(my $fh, '<', $filename) or die "Can't open $filename";
 while(<$fh>) {
-    if($_ =~ m/^(use|require|include) ((\w*::)*\w+)/) {
+    if($_ =~ m/^[ ]*(use|require|include) ((\w*::)*\w+)/) {
         my $mod_with_colons = $2; 
         next if ($mod_with_colons =~ /(strict|warn|POSIX)/);
         my $base = get_basedir();
@@ -21,7 +22,7 @@ while(<$fh>) {
         push @file_modules, $full_path;
     }   
 }
-
+print "(debug) file_modules $_\n" for @file_modules;
 my @modules;
 if ( !@file_modules ) { 
     print "Nothing found in the file provided, now searching \@INC...\n";
@@ -35,18 +36,28 @@ if ( !@modules ) {
     print "Nothing found anywhere :(\n";
 }
 
-foreach (@modules) {
-    my @found;
-    my $mod_full_path = $_ ;
-    open(my $mod_fh, '<', $mod_full_path) or die "Can't open $_";
-    while(<$mod_fh>) {
-        if($_ =~ m/sub $function_name/) {
-            push @found, $_; 
-            print "\nFound:\nvim $mod_full_path\n";
-            print "@found\n";
+populate_found();
+#print_if_empty();
+print_results();
+
+sub populate_found {
+    foreach (@modules) {
+        my $mod_full_path = $_ ;
+        open(my $mod_fh, '<', $mod_full_path) or die "Can't open $_";
+        while(<$mod_fh>) {
+            if($_ =~ m/sub $function_name/) {
+                $found{$_} = $mod_full_path; 
+            }   
         }   
+        close($mod_fh);
     }   
-    close($mod_fh);
+}
+
+sub print_results {
+    foreach ( keys %found ) { 
+        my $sub_declaration = $_; 
+        print "\nFound:\nvim $found{$sub_declaration}\n$sub_declaration\n";
+    }
 }
 
 sub get_basedir {
@@ -54,7 +65,7 @@ sub get_basedir {
     my @ret;
     foreach (@bases) {
         if (-d $_) { push @ret, $_ };
-    }   
+    }
     my $last = pop(@ret);
     return $last;
 }
