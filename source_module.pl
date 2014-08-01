5b63e7e635a7e5113f791a007221b8353323c01d
Marco <polo177@gmail.com>
2014-08-01 13:54:17 -0500

    improved: validate input modules in source file use valid values in mod array instead of partially search/replaced ones use @INC if not found in source file

--- source_module.pl
+++ source_module.pl
@@ -4,34 +4,45 @@ use warnings;
 
 my $filename = $ARGV[0];
 my $function_name = $ARGV[1];
-my @modules;
+my @file_modules;
 
 open(FH, $filename) or die "Can't open $filename";
 while(<FH>) {
     if($_ =~ m/(use|require|include) ((\w*::)*\w+)/) {
-    if ($2 =~ /(strict|warn)/) { next; }
-    my $base = get_basedir();
-        push @modules, "$base/$2.pm";
+        my $mod_with_colons = $2; 
+        next if ($mod_with_colons =~ /(strict|warn|POSIX)/);
+        my $base = get_basedir();
+        ( my $mod_with_slashes = $mod_with_colons ) =~ s/::/\//g ;
+        if ( ! -f "$base/$mod_with_slashes.pm" ) { 
+            print "$mod_with_colons not found: $base/$mod_with_slashes.pm\n";
+            next;
+        }   
+        my $full_path = "$base/$mod_with_slashes.pm";
+        push @file_modules, $full_path;
     }   
 }
-my @module_locations = map { s/::/\//g; $_; } @modules;
-#print "@module_locations\n";
-#print "$_\n" for @module_locations;
 
-#print "\n(debug) Done with collection\n\n";
+my @modules;
+if ( !@file_modules ) { 
+    print "Nothing found in the file provided, now searching \@INC...\n";
+    @modules = @INC; 
+}
+else {
+    @modules = @file_modules;
+}
 
-foreach (@module_locations) {
+if ( !@modules ) { 
+    print "Nothing found anywhere :(\n";
+}
+
+foreach (@modules) {
     my @found;
-#print "(debug) outside GFH, argv[1] is: $ARGV[1]\n";
-#print "(debug) dol_ is $_\n";
-    chomp(my $mod_full_path = $_) ;
-#print "(debug) mod_full_path is $mod_full_path\n";
+    my $mod_full_path = $_ ;
     open(GFH, $mod_full_path) or die "Can't open $_";
     while(<GFH>) {
-#print "(debug) inside GFH, argv[1] is: $ARGV[1]\n";
-        if($_ =~ m/sub $ARGV[1]/) {
+        if($_ =~ m/sub $function_name/) {
             push @found, $_; 
-            print "\nvim $mod_full_path\n";
+            print "\nFound:\nvim $mod_full_path\n";
             print "@found\n";
         }   
     }   
@@ -43,6 +54,7 @@ sub get_basedir {
     my @ret;
     foreach (@bases) {
         if (-d $_) { push @ret, $_ };
-    }
-    return "@ret";
+    }   
+    my $last = pop(@ret);
+    return $last;
 }
