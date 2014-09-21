3bf7e04f6cca4903acec2299c4b3f9190c2b5592
Marco Ferrufino <marco@Marcos-MacBook-Pro-2.local>
2014-09-20 21:32:45 -0500

    perl version of bash script. in development

---
+++ local_site_check.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Data::Dumper;
+
+my $dom = "numdb2.test";
+my $ip = "10.4.130.3";
+#system ("curl -s numdb2.test --resolve '$dom:80:$ip'");
+
+my $domain_list;
+my %trueuserdata;
+
+sub if_trueuserdomains {
+    my $filename = "/etc/trueuserdomains";
+    open my $fh,"<",$filename or die "failed to open file: $!";
+    %trueuserdata=map{chomp;split ": "} <$fh>;
+
+    for my $dom (keys %trueuserdata) {
+        #print "$dom\n";
+        system ("curl -s $dom --resolve '$dom:80:$ip'");
+    }
+    #return %trueuserdata;
+};
+
+if_trueuserdomains();
+#print Dumper(\%trueuserdata);
